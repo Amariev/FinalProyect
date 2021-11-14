d148d63a3a2686da260c28f7125fac9da63c0aa4
Camila <camilaclary@gmail.com>
2021-11-14 15:29:49 -0500

    fix

--- include/maze.h
+++ include/maze.h
@@ -1,51 +1,22 @@
-<<<<<<< HEAD
 #include <iostream>
+using namespace std;
 
-class Maze{
+class Maze {
 
-    public:
-    int row;
-    int col;
-    int density;
+public:
+  int row;
+  int col;
+  int density;
 
-    Maze(int, int,int);
+  // maze(int, int,int);
 
-    void inicializarMatriz();
-    void laberinto();
-    void drawMaze();
-};
-//constructor 
-Maze::Maze(int filas,int columnas, int densidad)
-{
+  void inicializarMatriz();
+  void laberinto();
+  void drawMaze();
+  // constructor
+  Maze(int filas, int columnas, int densidad) {
     row = filas;
     col = columnas;
     density = densidad;
-}
-
-//Métodos
-=======
-#include <iostream>
-
-class Maze{
-
-    public:
-    int row;
-    int col;
-    int density;
-
-    // maze(int, int,int);
-
-    void inicializarMatriz();
-    void laberinto();
-    void drawMaze();
-//constructor 
-    Maze(int filas,int columnas, int densidad)
-{
-    row = filas;
-    col = columnas;
-    density = densidad;
-}
-
-//Métodos
+  }
 };
->>>>>>> b778493f6433b39311c8badd3da7bf361552fc59
