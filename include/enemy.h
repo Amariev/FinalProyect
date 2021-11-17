d3e4e0159824f06e1dac7dfb81026ad6f9328e6f
CamiaCL <camilaclary@gmail.com>
2021-11-16 23:26:45 -0500

    normal enemy

--- include/enemy.h
+++ include/enemy.h
@@ -10,3 +10,10 @@ public:
     Enemy(int row, int col, char symbol, int lives = 1, bool alive = true, int speed = 1);
 };
 #endif
+
+class Normal_Enemy : public Enemy {
+public:
+    bool alive;
+
+    Normal_Enemy(int row, int col, char symbol, int lives = 1, bool alive = true, int speed = 1);
+};
