c7118be7e0c5ca4f99d1e04596faf2f5ef7feaf8
Karol525 <Karol.quispe@ucsp.edu.pe>
2021-11-24 22:15:50 -0500

    traspaso de paredes (error)

--- src/personaje.cpp
+++ src/personaje.cpp
@@ -44,6 +44,7 @@ Directions Personaje::getDirection() { return direction; }
 
 //
 
+/*
 void Personaje::move() {
   switch (direction) {
   case Directions::UP:
@@ -62,6 +63,23 @@ void Personaje::move() {
     break;
   }
 }
+*/
+
+void Personaje::move() {
+  if(direction == UP && getPos().Y > 1){
+    pos.Y -= speed;
+  }
+  else if(direction == DOWN && getPos().Y < 15){
+    pos.Y += speed;
+  }
+  else if(direction == RIGHT && getPos().X < 15){
+    pos.X += speed;
+  }
+  else if(direction == LEFT && getPos().X > 1){
+    pos.X-= speed;
+  } 
+}
+
 
 void Personaje::printAttributes() {
   std::cout << "Symbol: " << symbol << std::endl;
