c3a9c3e9543baff39ace1e9ad56b168c9e045444
CamiaCL <camilaclary@gmail.com>
2021-11-26 17:13:47 -0500

    ✨  configure cmake list

---
+++ include/basic.h
@@ -0,0 +1,59 @@
+#ifndef BASIC_H_
+#define BASIC_H_
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+
+#include "../lib/conio.h"
+
+#include "ability.h"
+#include "bomb.h"
+#include "button.h"
+#include "character.h"
+#include "end-game.h"
+#include "enemy.h"
+#include "engine.h"
+#include "entity.h"
+#include "game-over.h"
+#include "game.h"
+#include "hero.h"
+#include "interface.h"
+#include "level.h"
+#include "main-menu.h"
+#include "menu.h"
+#include "moveable.h"
+#include "next-level.h"
+#include "normal.h"
+#include "power-up.h"
+#include "smart.h"
+#include "stable.h"
+#include "stats.h"
+#include "tile.h"
+
+class Ability;
+class Bomb;
+class Button;
+class Character;
+class EndGame;
+class Enemy;
+class Engine;
+class Entity;
+class GameOver;
+class Game;
+class Hero;
+class Interface;
+class Level;
+class MainMenu;
+class Menu;
+class Moveable;
+class NextLevel;
+class Normal;
+class PowerUp;
+class Smart;
+class Stable;
+class Stats;
+class Tile;
+
+#endif
